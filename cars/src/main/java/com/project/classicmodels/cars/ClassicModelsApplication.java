ca834e6b4a15e3a71b6703076f3f9616388b001e
Mayank Sharma <mayank.1996.2014@gmail.com>
2020-02-14 12:06:47 +0530

    Initial Project Setup

---
+++ cars/src/main/java/com/project/classicmodels/cars/ClassicModelsApplication.java
@@ -0,0 +1,13 @@
+package com.project.classicmodels.cars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassicModelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassicModelsApplication.class, args);
+	}
+
+}
---
+++ cars/src/test/java/com/project/classicmodels/cars/ClassicModelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.classicmodels.cars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassicModelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
